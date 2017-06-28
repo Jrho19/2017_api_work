383fbf0f1b049ad84565ab3fc24b99260ef78cb9
John Rhoden <jrho19.jr@gmail.com>
2017-06-28 05:46:30 -0400

    test updates and module tweaks

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -17,6 +17,21 @@ RSpec.describe 'Todos API', type: :request do
     end
   end
 
+  describe 'GET /todos/:id' do
+    before { get "/todos/#{todo_id}" }
+
+    context 'when the record exists' do
+      it 'returns the todo' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(todo_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+
   context 'when the record does not exist' do
     let(:todo_id) { 100 }
 
@@ -29,6 +44,7 @@ RSpec.describe 'Todos API', type: :request do
     end
   end
 end
+
 
 describe 'POST /todos' do
   let(:valid_attributes) { { title: 'Learn Elm', created_by: 1 } }
@@ -43,41 +59,43 @@ describe 'POST /todos' do
     it 'returns a status code 201' do
       expect(response).to have_http_status(201)
     end
+  end
 
-    context 'when the request is invalid' do
-      before { post '/todos', params: { title: 'Foobar' } }
-    end
+      context 'when request is invalid' do
+        before { post '/todos', params: { title: 'Foobar' } }
 
-    it 'returns status code 422' do
-      expect(response).to have_http_status(422)
-    end
 
-    it 'returns a validation failure message' do
-      expect(response.body).to match(/Validatio failed: Created by can't be black/)
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body).to match(/Validation failed: Created by can't be blank/)
+      end
     end
   end
-end
 
-describe 'PUT /todos/:id' do
-  let(:valid_attributes) { { title: 'Shopping' } }
+  describe 'PUT /todos/:id' do
+    let(:valid_attributes) { { title: 'Shopping' }.to_json }
 
-  context 'when record exists' do
-    before { put "/todos/#{todo_id}", params: valid_attributes }
+    context 'when record exists' do
+      before { put "/todos/#{todo_id}", params: valid_attributes }
 
-    it 'updates the record' do
-      expect(response.body).to be_empty
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
     end
+  end
 
-    it 'returns status code 204' do
+  describe 'DELETE /todos/:id' do
+    before { delete "/todos/#{todo_id}" }
+
+    it "returns status code 204" do
       expect(response).to have_http_status(204)
     end
-  end
-end
-
-describe 'DELETE /todos/:id' do
-  before { delete "/todos/#{todo_id}" }
-
-  it "returns status code 204" do
-    expect(response).to have_http_status(204)
   end
 end
